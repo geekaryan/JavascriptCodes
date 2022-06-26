8bf6e9340df77251fe336fa6974c25b6d380c292
sudoaptrana <aryan.rana2021@vitbhopal.ac.in>
2022-06-26 10:25:02 +0530

    26-Jun

--- Looping code/scripts/main.js
+++ Looping code/scripts/main.js
@@ -1,24 +1,53 @@
-const btn = document.querySelector('button');
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
 
-document.addEventListener('DOMContentLoaded', () => {
-    canvas.width = document.documentElement.clientWidth;
-    canvas.height = document.documentELement.clientHeight;
-})
 
-function random(number){
-    return Math.floor(Math.random()*number);
+//for...of loops
+
+const cats = ['Leopard', 'Serval', 'Jaguar','Tiger', 'Caracal', 'Lion'];
+
+for(const cat of cats) {
+    console.log(cat);
 }
 
-function draw(){
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    for (let i = 0; i < 100; i++){
-        ctx.beginPath();
-        ctx.fillStyle = 'rgba(255,0,0,0.5');
-        ctx.arc(random(canvas.width), random(canvas.heigth), random(50), 0, 2 * Math.PI);
-        ctx.fill();
-    }
+//map() used to do something to each item in  a collectin and create a new collection containig the changed item
+
+function toUpper(string){
+    return string.toUpperCase();
 }
 
-btn.addEventListener('click',draw);
+const upperCats = cats.map(toUpper);
+
+console.log(upperCats);
+
+//filter() is used to test each item in a collection, and create a new collection containing only items that match
+
+function lCat(cat){
+    return cat.startsWith('L');
+}
+
+const filtered = cats.filter(lCat);
+
+console.log(filtered);
+
+//standard for loop 
+//for (intializer; condition; finalexpression){}
+
+//for loop examples 
+
+const results = document.querySelector('#results');
+
+const results = document.querySelector('#results');
+
+function calculate() {
+  for (let i = 1; i < 10; i++) {
+    const newResult = `${i} x ${i} = ${i * i}`;
+    results.textContent += `${newResult}\n`;
+  }
+  results.textContent += '\n...finished!';
+}
+
+const calculateBtn = document.querySelector('#calculate');
+const clearBtn = document.querySelector('#clear');
+
+calculateBtn.addEventListener('click', calculate);
+clearBtn.addEventListener('click', () => results.textContent = '');
+
